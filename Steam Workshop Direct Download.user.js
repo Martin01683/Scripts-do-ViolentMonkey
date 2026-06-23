e027504c1668f1fa7e44dbb691309f3bde0f4b15
Martin01683 <292215010+Martin01683@users.noreply.github.com>
2026-06-23 09:57:53 +0000

    Substitui ▼ pelo ícone ChevronDown do Lucide Icons no botão dropdown

--- Steam Workshop Direct Download.user.js
+++ Steam Workshop Direct Download.user.js
@@ -1085,7 +1085,7 @@
                 if (validItems.length > 0) {
                     // Armazena a estrutura do menu no DOM (data-attribute) para ser lida quando clicado
                     const dropdownData = escapeHTML(JSON.stringify(validItems));
-                    html += `<button class="swdd-custom-btn ${cClass} ${config.stateClass} swdd-btn-arrow" data-dropdown-items="${dropdownData}">▼</button>`;
+                    html += `<button class="swdd-custom-btn ${cClass} ${config.stateClass} swdd-btn-arrow" data-dropdown-items="${dropdownData}"><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display:inline-block;vertical-align:middle;"><path d="m6 9 6 6 6-6"/></svg></button>`;
                 }
             }
 
